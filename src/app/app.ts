71ecda04ed525058e69b4761cc670d5acbc515c4
Patrick Thadeus <patrickichu058@gmail.com>
2025-09-21 05:10:43 +0100

    header components

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation-component/navigation-component";
 import { CardComponent } from "./card-component/card-component";
 import { MiddleComponent } from "./middle-component/middle-component";
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent, CardComponent, MiddleComponent],
+  imports: [RouterOutlet, NavigationComponent, CardComponent, MiddleComponent, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
