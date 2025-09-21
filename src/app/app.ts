5d47c0588e5f7fb023b8596bc566d70e5dfbf13b
Patrick Thadeus <patrickichu058@gmail.com>
2025-09-21 19:45:20 +0100

    doctors section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation-component/navigation-component";
 import { CardComponent } from "./card-component/card-component";
 import { MiddleComponent } from "./middle-component/middle-component";
@@ -8,7 +8,7 @@ import { FooterComponent } from "./footer-component/footer-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent, Home, FooterComponent],
+  imports: [NavigationComponent, Home, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
