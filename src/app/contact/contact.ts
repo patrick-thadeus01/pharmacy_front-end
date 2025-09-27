e896612f0a5959c0872121cf96744e7c99ff77b4
Patrick Thadeus <patrickichu058@gmail.com>
2025-09-27 23:43:28 +0100

    home page

---
+++ src/app/contact/contact.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Home } from "../home/home";
+
+@Component({
+  selector: 'app-contact',
+  imports: [Home],
+  templateUrl: './contact.html',
+  styleUrl: './contact.css'
+})
+export class Contact {
+
+}
