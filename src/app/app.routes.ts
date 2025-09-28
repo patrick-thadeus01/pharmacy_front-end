5cd5706b36489973ca6b0253514e6ca29a5690c4
Patrick Thadeus <patrickichu058@gmail.com>
2025-09-28 15:12:34 +0100

    configuring the app.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Route pour la page d'accueil
+  { path: 'home', component: Home, title: 'Home - KMD-Pharmacy' },
+  {path: 'contact', component: Contact, title: 'Contact - KMD-Pharmacy' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet, RouterLink, provideRouter } from '@angular/router';
 // import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation-component/navigation-component";
 import { CardComponent } from "./card-component/card-component";
 import { MiddleComponent } from "./middle-component/middle-component";
 import { Home } from "./home/home";
 import { FooterComponent } from "./footer-component/footer-component";
+import { Contact } from "./contact/contact";
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [NavigationComponent, Home, FooterComponent],
   templateUrl: './app.html',
